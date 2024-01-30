04bc72bfcdc4fa2d1e1bcaaa7670336adc1f7a70
MMDora2 <m.mihicsdora@gmail.com>
2024-01-30 09:59:37 +0100

    Task1: Extend the list with filter features

--- client/src/Components/EmployeeTable/EmployeeTable.jsx
+++ client/src/Components/EmployeeTable/EmployeeTable.jsx
@@ -1,36 +1,81 @@
 import { Link } from "react-router-dom";
 import "./EmployeeTable.css";
+import { useState } from "react";
 
-const EmployeeTable = ({ employees, onDelete }) => (
-  <div className="EmployeeTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Level</th>
-          <th>Position</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee._id}>
-            <td>{employee.name}</td>
-            <td>{employee.level}</td>
-            <td>{employee.position}</td>
-            <td>
-              <Link to={`/update/${employee._id}`}>
-                <button type="button">Update</button>
-              </Link>
-              <button type="button" onClick={() => onDelete(employee._id)}>
-                Delete
-              </button>
-            </td>
+const EmployeeTable = ({ employees, onDelete }) => {
+  const [searched, setSearched] = useState("");
+
+  function handleSearch(e) {
+    setSearched(e.target.value);
+    // console.log(e.target.value)
+  }
+
+  return (
+    <div className="EmployeeTable">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Position</th>
+            <th>
+              <input
+                onChange={handleSearch}
+                placeholder="Search by level or position"
+              ></input>
+            </th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {!searched
+            ? employees.map((employee) => (
+                <tr key={employee._id}>
+                  <td>{employee.name}</td>
+                  <td>{employee.level}</td>
+                  <td>{employee.position}</td>
+                  <td>
+                    <Link to={`/update/${employee._id}`}>
+                      <button type="button">Update</button>
+                    </Link>
+                    <button
+                      type="button"
+                      onClick={() => onDelete(employee._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            : employees.map((employee) =>
+                employee.position
+                  .toLowerCase()
+                  .includes(searched.toLowerCase()) ||
+                employee.level
+                  .toLowerCase()
+                  .includes(searched.toLowerCase()) ? (
+                  <tr key={employee._id}>
+                    <td>{employee.name}</td>
+                    <td>{employee.level}</td>
+                    <td>{employee.position}</td>
+                    <td>
+                      <Link to={`/update/${employee._id}`}>
+                        <button type="button">Update</button>
+                      </Link>
+                      <button
+                        type="button"
+                        onClick={() => onDelete(employee._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ) : null
+              )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default EmployeeTable;
