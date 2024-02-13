1552ec1bf5c335fe1b44f8eec1983a9069f4d91d
MMDora2 <m.mihicsdora@gmail.com>
2024-02-13 10:04:43 +0100

    Practice:Total Bonuses

--- client/src/Pages/Bonuses.jsx
+++ client/src/Pages/Bonuses.jsx
@@ -1,36 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Bonuses = () => {
   const { employeeId } = useParams();
-  const [bonusValue, setBonusValue] = useState('');
+  const [bonusValue, setBonusValue] = useState("");
   const [employee, setEmployee] = useState(null);
 
   useEffect(() => {
     fetch(`/api/employees/${employeeId}`)
       .then((response) => response.json())
       .then((data) => setEmployee(data))
-      .catch((error) => console.error('Error:', error));
+      .catch((error) => console.error("Error:", error));
   }, [employeeId]);
 
   const handleBonusChange = (event) => {
     setBonusValue(event.target.value);
   };
+  const totalBonuses = () => {
+    if (!employee.bonuses) return 0;
+    return employee.bonuses.reduce((total, bonus) => total + bonus.value, 0);
+  };
 
   const handleSaveBonus = () => {
     fetch(`/api/bonus/${employeeId}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ value: bonusValue }),
     })
       .then((response) => response.json())
       .then((data) => {
         setEmployee(data);
-        setBonusValue('');
+        setBonusValue("");
       })
-      .catch((error) => console.error('Error saving bonus:', error));
+      .catch((error) => console.error("Error saving bonus:", error));
   };
 
   return (
@@ -55,6 +59,7 @@ const Bonuses = () => {
               <li key={index}>{bonus.value}</li>
             ))}
           </ul>
+          <h3><p>Total Bonuses:{totalBonuses()}</p></h3>
         </>
       )}
     </div>
